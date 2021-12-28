af09e8c5045ec194984c7e38b529c48706181b9b
masoniis <bott.m@comcast.net>
2021-12-28 16:44:33 -0700

    Commit on 'main'

--- components/Header.js
+++ components/Header.js
@@ -5,23 +5,23 @@ const Header = () => {
   return (
     <header className="bg-brown text-zinc-200">
       <div className="mx-auto flex flex-wrap p-5 items-center">
-        <svg width="100" height="50">
+        {/* <svg width="100" height="50">
           <circle cx="50" cy="50" r="40" stroke="green" fill="yellow" />
-        </svg>
+        </svg> */}
 
         <nav className="md:ml-auto flex items-center justify-between">
-          <a href="/" className="mr-5 hover:text-white">
-            Home Page
-          </a>
-          <a href="/page2" className="mr-5 hover:text-white">
-            Second Page
-          </a>
-          <a href="/page3" className="mr-5 hover:text-white">
-            Third Page
-          </a>
-          <a href="/page4" className="mr-5 hover:text-white">
-            Fourth Page
-          </a>
+          <Link href="/">
+            <a className="mr-5 hover:text-white">Home Page</a>
+          </Link>
+          <Link href="/page2" className="mr-5 hover:text-white">
+          <a className="mr-5 hover:text-white">Second Page</a>
+          </Link>
+          <Link href="/page3">
+          <a className="mr-5 hover:text-white">Third Page</a>
+          </Link>
+          <Link href="/page4">
+          <a className="mr-5 hover:text-white">Fourth Page</a>
+          </Link>
         </nav>
 
       </div>
