669176387d47e935085d7514e0cd7d911ff329f6
masoniis <bott.m@comcast.net>
2021-12-28 16:51:27 -0700

    commit on 'main'

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-green-500 bg-slate-600 text-center">
-        Hello World github test 2{" "}
+        Hello World VERCEL TEST
       </h1>
     </div>
   );
