e471df6a743c6cd25a6385de8ae205b662ec8bf4
masoniis <58895787+masoniis@users.noreply.github.com>
2021-12-28 16:13:36 -0700

    Testing pull/push

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import Head from 'next/head'
+import Head from "next/head";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-green-500 bg-slate-600 text-center">Hello World</h1>
+      <h1 className="text-green-500 bg-slate-600 text-center">
+        Hello World github test{" "}
+      </h1>
     </div>
-  )
+  );
 }
