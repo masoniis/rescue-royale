6db61a77fa17f1250f1c83dec49a928019a23499
masoniis <bott.m@comcast.net>
2021-12-28 16:38:47 -0700

    Commit on 'main'

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="bg-brown text-zinc-200">
       <div className="mx-auto flex flex-wrap p-5 items-center">
         <svg width="100" height="50">
-          <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+          <circle cx="50" cy="50" r="40" stroke="green" fill="yellow" />
         </svg>
 
         <nav className="md:ml-auto flex items-center justify-between">
