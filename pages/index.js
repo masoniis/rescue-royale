99382b94db5e1f24406da9759d73ab147d55bc13
masoniis <bott.m@comcast.net>
2021-12-28 16:03:07 -0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-green-500 bg-slate-600 text-center">Hellow World</h1>
+      <h1 className="text-green-500 bg-slate-600 text-center">Hello World</h1>
     </div>
   )
 }
