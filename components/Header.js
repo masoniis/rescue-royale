24746089dbd1303884615a37549309cf4159ba0d
masoniis <bott.m@comcast.net>
2021-12-29 14:37:22 -0700

    commit on 'main'

--- components/Header.js
+++ components/Header.js
@@ -3,25 +3,25 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="bg-brown text-zinc-200">
-      <div className="mx-auto flex flex-wrap p-5 items-center">
-        {/* <svg width="100" height="50">
-          <circle cx="50" cy="50" r="40" stroke="green" fill="yellow" />
-        </svg> */}
-        <p>Rescue Royale</p>
+    <header className="bg-zinc-200 text-gray-500 p-6">
+      <div className="container flex mx-auto flex-wrap items-center">
 
+        {/* Flex Item 1 */}
+        <p>SVG logo goes here</p>
+
+        {/* Flex Item 2 */}
         <nav className="md:ml-auto flex items-center justify-between">
           <Link href="/">
-            <div className="mr-5 hover:text-white hover:cursor-pointer">Home Page</div>
+            <div className="mr-5 hover:text-gray-700 hover:cursor-pointer">Home Page</div>
           </Link>
           <Link href="/page2">
-            <div className="mr-5 hover:text-white hover:cursor-pointer">Second Page</div>
+            <div className="mr-5 hover:text-gray-700 hover:cursor-pointer">Second Page</div>
           </Link>
           <Link href="/page3">
-            <div className="mr-5 hover:text-white hover:cursor-pointer">Third Page</div>
+            <div className="mr-5 hover:text-gray-700 hover:cursor-pointer">Third Page</div>
           </Link>
           <Link href="/contact-us">
-            <div className="mr-5 hover:text-white hover:cursor-pointer">Contact Us</div>
+            <div className="mr-5 hover:text-gray-700 hover:cursor-pointer">Contact Us</div>
           </Link>
         </nav>
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
-    <div className="text-green-500 bg-slate-600 text-center">
+    <body className="h-screen text-green-900 bg-zinc-200 text-center">
       <h1 className="p-11">
         Hello World EPIC TEST 4
       </h1>
-    </div>
+    </body>
   );
 }
