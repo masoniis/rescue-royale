4898b03e549a82243e81baf6ec0cb86781c88e97
masoniis <bott.m@comcast.net>
2021-12-29 13:49:21 -0700

    commit on 'main'

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,34 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-green-500 bg-slate-600 text-center">
+    <div className="text-green-500 bg-slate-600 text-center">
+      <h1>
         Hello World EPIC TEST 4
       </h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <p> Hi</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <p> I am a long body with a lot of content</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <p> so that the footer looks cooler</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <p> Wow doesn't that footer look so coooooool</p>
     </div>
   );
 }
