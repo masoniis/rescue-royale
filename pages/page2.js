f490d0bbe17aca3e52a036f4929e87d80a403734
masoniis <bott.m@comcast.net>
2021-12-29 20:00:38 -0700

    commit on 'main'

--- pages/page2.js
+++ pages/page2.js
@@ -1,6 +1,6 @@
 const Page2 = () => {
     return (
-        <body className="min-h-screen bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text text-center">
+        <body className="min-h-screen bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text transition-colors duration-500 text-center">
             <div>
                 <h1>
                     I am page 2!
