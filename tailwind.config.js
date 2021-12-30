ba4e0807a12e3478b75a334f3ea1760071f9ba7d
masoniis <bott.m@comcast.net>
2021-12-29 20:47:18 -0700

    commit on 'main'

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,9 @@ module.exports = {
         'dark-text': '#a8a29e',
         'dark-hover': '#f4ebe5',
 
-        'light-bg': '#e0e0e3',
+        'light-bg': '#e4e4e7',
         'light-text': '#374151',
-        'light-hover': '#374151'
+        'light-hover': '#030304'
       },
     },
   },
